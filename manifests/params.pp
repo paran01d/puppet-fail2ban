98c677151fa00a47ed50893035e92440b00222ae
Thomas Sutton <me@thomas-sutton.id.au>
2012-05-11 11:37:13 +0800

    Use config override file instead of re-writing
    This makes the package manager much happier.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,14 @@
 
 class fail2ban::params {
 
+	$packages = ['fail2ban']
+
+	$services = ['fail2ban']
+
 	$jail_local = '/etc/fail2ban/jail.local'
+
+	$destemail = ''
+
+	$jails = ['ssh-ddos', 'ssh']
+
 }
